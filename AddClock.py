c4b305396fdeb30f31f2ff06aa2aa13b9ae2a97a
thelittlelab <87830139+thelittlelab@users.noreply.github.com>
2021-11-14 12:42:34 -0500

    added frame rate, changed to timecode

--- AddClock.py
+++ AddClock.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 import os
-#import ffmpeg
+import cv2
          
 ffmpegCommand1 = '''ffmpeg -y -i '''   
-ffmpegCommand2 = ''' -vf "drawtext=fontfile=/tmp/UbuntuMono-B.ttf:fontsize=36:fontcolor=yellow:box=1:boxcolor=black@0.4:text='\: %{pts\:hms}'" '''
-
+ffmpegCommand2 = ''' -vf "drawtext=timecode='00\:00\:00\:00':rate='''
+ffmpegCommand3 = ''':fontsize=48:fontcolor=white:box=1:boxborderw=6:boxcolor=black@0.75:x=0:y=0" -c:a copy '''
 
 pathlist = Path('.').glob('*.mp4')
 
@@ -12,5 +12,6 @@ for path in pathlist:
      path_in_str = str(path)
      name, ext = os.path.splitext(path_in_str)
      output = name + '_Clock' + ext
-
-     os.system(ffmpegCommand1 + path_in_str + ffmpegCommand2 + output)
+     cap=cv2.VideoCapture(path_in_str)
+     fps = str(cap.get(cv2.CAP_PROP_FPS))
+     os.system(ffmpegCommand1 + path_in_str + ffmpegCommand2 + fps + ffmpegCommand3 + output)
